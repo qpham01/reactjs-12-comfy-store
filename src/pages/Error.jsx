b67997a78b503a1a4118c4a9af59a11370d33136
Quoc Pham <quoc_pham@msn.com>
2025-04-06 16:12:38 -0500

    Error page solution.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,23 +1,31 @@
-import { Link, useRouteError } from 'react-router-dom';
-import img from '../assets/not-found.svg';
-
-const ErrorPage = () => {
+import { useRouteError, Link } from 'react-router-dom';
+const Error = () => {
   const error = useRouteError();
   console.log(error);
-  if (error.status === 404) {
+  if (error.status === 404)
     return (
-      <div className='text-center'>
-        <img src={img} alt='not found' className='mx-auto' />
-        <h3>Ohh! Page Not Found</h3>
-        <p>We can't seem to find the page you're looking for</p>
-        <Link to='/'>Back Home</Link>
-      </div>
+      <main className='grid min-h-[100vh] place-items-center px-8 '>
+        <div className='text-center'>
+          <p className='text-9xl font-semibold text-primary'>404</p>
+          <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+            Page not found
+          </h1>
+          <p className='mt-6 text-lg leading-7 '>
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className='mt-10 '>
+            <Link to='/' className='btn btn-secondary'>
+              Go back home
+            </Link>
+          </div>
+        </div>
+      </main>
     );
-  }
+
   return (
-    <div>
-      <h3>Something went wrong</h3>
-    </div>
+    <main className='grid min-h-[100vh] place-items-center px-8 '>
+      <h4 className='text-center font-bold text-4xl'>there was an error... </h4>
+    </main>
   );
 };
-export default ErrorPage;
+export default Error;
