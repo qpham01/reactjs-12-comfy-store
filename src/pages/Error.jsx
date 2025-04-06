130546e1f29c6d418334d1df2ff12989b35ea50d
Quoc Pham <quoc_pham@msn.com>
2025-04-06 16:11:48 -0500

    self-impl error page and error handling.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,23 @@
-const Error = () => {
-  return <div>Error</div>;
+import { Link, useRouteError } from 'react-router-dom';
+import img from '../assets/not-found.svg';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <div className='text-center'>
+        <img src={img} alt='not found' className='mx-auto' />
+        <h3>Ohh! Page Not Found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to='/'>Back Home</Link>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h3>Something went wrong</h3>
+    </div>
+  );
 };
-export default Error;
+export default ErrorPage;
