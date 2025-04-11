90f188e5598df63883676c085f0d1bb43724202b
Quoc Pham <quoc_pham@msn.com>
2025-04-10 19:05:50 -0500

    Added loading indicator when loading products.

--- src/app-specific/components/index.js
+++ src/app-specific/components/index.js
@@ -1,2 +1,3 @@
 export { default as ProductGrid } from './ProductGrid';
 export { default as ProductList } from './ProductList';
+export { default as Loading } from './Loading';
--- src/app-specific/pages/HomeLayout.jsx
+++ src/app-specific/pages/HomeLayout.jsx
@@ -1,15 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { Loading } from '../components';
 import { Header, StoreNavbar } from '../../reusable/sections';
 import links from '../data/links';
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
+
   return (
     <>
       <Header />
       <StoreNavbar links={links} />
-      <section className='align-element py-10'>
-        <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className='align-element py-10'>
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
